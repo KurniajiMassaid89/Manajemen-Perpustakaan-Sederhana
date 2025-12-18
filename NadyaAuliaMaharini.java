185447c5998192046d21de39d16131ccb6dbde26
Nadya Aulia Maharani <nadyamaharani205@gmail.com>
2025-12-18 18:52:38 +0700

    Menampilkan Perkenalan Diri

---
+++ NadyaAuliaMaharini.java
@@ -0,0 +1,5 @@
+public class NadyaAuliaMaharini {
+    public static void main(String[] args) {
+        System.out.println("Saya Nadya Aulia Maharani dengan NIM 1324063 dari kelas SA03");
+    }
+}
