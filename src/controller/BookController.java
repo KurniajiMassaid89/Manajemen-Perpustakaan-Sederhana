54d7ca1a88f7b2eb000d0a0bc6d9f1ba999d7438
KurniajiMassaid89 <said.mk072@gmail.com>
2025-12-21 22:24:54 +0700

    File Intregation

---
+++ src/controller/BookController.java
@@ -0,0 +1,19 @@
+package src.controller;
+
+import backend.*;
+
+public class BookController {
+    private BookRepository repo = new BookRepository();
+
+    public BookRepository getRepo() {
+        return repo;
+    }
+
+    public void pinjamBuku(Book b) throws Exception {
+        b.pinjam();
+    }
+
+    public void kembalikanBuku(Book b) {
+        b.kembalikan();
+    }
+}
