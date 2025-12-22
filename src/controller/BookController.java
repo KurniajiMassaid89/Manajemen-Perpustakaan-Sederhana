7312588fd93dfb3f1da5a0dade10e82a19ab0d39
KurniajiMassaid89 <said.mk072@gmail.com>
2025-12-22 16:51:30 +0700

    Update Controller

--- src/controller/BookController.java
+++ src/controller/BookController.java
@@ -1,7 +1,5 @@
 package controller;
 
-import java.util.ArrayList;
-
 import backend.Book;
 import backend.BookRepository;
 
@@ -9,27 +7,15 @@ public class BookController {
 
     private BookRepository repo = new BookRepository();
 
-    public void tambahBuku(String kode, String judul, int stok) {
-        repo.tambah(new Book(kode, judul, stok));
+    public BookRepository getRepo() {
+        return repo;
     }
 
-    public void hapusBuku(int index) {
-        repo.hapus(index);
+    public void pinjamBuku(Book b) throws Exception {
+        b.pinjam();
     }
 
-    public void pinjamBuku(int index) throws Exception {
-        repo.getAll().get(index).pinjam();
-    }
-
-    public void kembalikanBuku(int index) {
-        repo.getAll().get(index).kembalikan();
-    }
-
-    public ArrayList<Book> getAllBuku() {
-        return repo.getAll();
-    }
-
-    public Book cariBuku(String keyword) {
-        return repo.cari(keyword);
+    public void kembalikanBuku(Book b) {
+        b.kembalikan();
     }
 }
