f5685982d858f672c68c7d486bbfd0d01be29222
KurniajiMassaid89 <said.mk072@gmail.com>
2025-12-22 13:48:11 +0700

    Fix package after backend merge

--- src/controller/BookController.java
+++ src/controller/BookController.java
@@ -1,19 +1,35 @@
-package src.controller;
+package controller;
 
-import backend.*;
+import java.util.ArrayList;
+
+import backend.Book;
+import backend.BookRepository;
 
 public class BookController {
+
     private BookRepository repo = new BookRepository();
 
-    public BookRepository getRepo() {
-        return repo;
+    public void tambahBuku(String kode, String judul, int stok) {
+        repo.tambah(new Book(kode, judul, stok));
     }
 
-    public void pinjamBuku(Book b) throws Exception {
-        b.pinjam();
+    public void hapusBuku(int index) {
+        repo.hapus(index);
     }
 
-    public void kembalikanBuku(Book b) {
-        b.kembalikan();
+    public void pinjamBuku(int index) throws Exception {
+        repo.getAll().get(index).pinjam();
+    }
+
+    public void kembalikanBuku(int index) {
+        repo.getAll().get(index).kembalikan();
+    }
+
+    public ArrayList<Book> getAllBuku() {
+        return repo.getAll();
+    }
+
+    public Book cariBuku(String keyword) {
+        return repo.cari(keyword);
     }
 }
